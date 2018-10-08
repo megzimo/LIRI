7ba99d09db99ccc7865ba436d1093c1b067abfd3
megzimo <40956115+megzimo@users.noreply.github.com>
2018-10-08 18:04:53 -0500

    movie-this working

--- liri.js
+++ liri.js
@@ -13,23 +13,36 @@ let inquirer = require('inquirer');
 let Spotify = require('node-spotify-api');
 
 let header = "~*~*~*~*~*~*~*~*~*~*~ Your Results ~*~*~*~*~*~*~*~*~*~*~";
+let inputQ = process.argv[2];
 
 
 //////////////////////////////////////// OMDB Movie Request ////////////////////////////////////////
-let request = require("request");
-let movie = process.argv[2];
-queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+switch(inputQ){
+    case "movie-this":
+        let movie = process.argv[3];
+        queryURL = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+        
+        request(queryURL, function (error, response, body) {
+            if(!error && response.statusCode === 200) {
+                let json = JSON.parse(body);
+                let movieReturn = "\n" + header + "\n" + "\n \t Title: " + json.Title + "\n \t Release Year: " + json.Year + "\n \t IMDb Rating: " + json.Title + "\n \t Rotten Tomatoes: " + json.Ratings[1] + "\n \t Country: " + json.Country + "\n \t Language: " + json.Language + "\n \t Plot: " + json.Plot + "\n \t Cast: "  + json.Actors + "\n";
+        
+                console.log(movieReturn);
+            };
+        });
+};
 
-request(queryURL, function (error, response, body) {
-    if(!error && response.statusCode === 200) {
-        let json = JSON.parse(body);
 
-        console.log("The movie plot is: " + json.Plot)
-    }
-})
+//////////////////////////////////////// Bands In Town ////////////////////////////////////////
+// var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=trilogy";
 
-//////////////////////////////////////// Next Section ////////////////////////////////////////
-
+//     request(queryURL, function (err, response, body) {
+//         if (error) console.log(err);
+//         var result  =  JSON.parse(body)[0];
+//         console.log("Venue name " + result.venue.name);
+//         console.log("Venue location " + result.venue.city);
+//         console.log("Date of Event " +  moment(result.datetime).format("MM/DD/YYYY"));
+//     });
 
 //////////////////////////////////////// Next Section ////////////////////////////////////////
 
